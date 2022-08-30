6338e355abbbd7cd32e7ac62e05ed2330dd05b67
PSNAppZ <thepsnarayanan@gmail.com>
2022-08-30 19:45:45 +0530

    Fix solution CF

--- CF/YES or YES?/solution.cpp
+++ CF/YES or YES?/solution.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
-
+ 
 int main() {
-    int tests;
-    string s[tests];
+   int tests;
    cin>>tests;
-    for (int i=0; i<tests; i++){
-       cin>>s[i];  
-        string temp;
-        if ( s[i] == "yes"){
-           cout<<"YES"<<endl;
-        }
-        else{
-           cout<<"NO"<<endl;
-        }
-    }
-
-    return 0;
+   for (int i=0; i<tests; i++){
+      string s;
+      cin>>s; 
+      transform(s.begin(), s.end(), s.begin(), ::tolower);
+      if ( s == "yes"){
+         cout<<"YES"<<endl;
+      }
+      else{
+         cout<<"NO"<<endl;
+      }
+   }
+ 
+   return 0;
 }
